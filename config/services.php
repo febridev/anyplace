9eff5716d5825d1119b18b40c507a5fc7a8ae70d
abidin13 <febriyantabidin@gmail.com>
2015-06-07 22:09:14 +0700

    commit 060715, versi 0

---
+++ config/services.php
@@ -0,0 +1,37 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Third Party Services
+	|--------------------------------------------------------------------------
+	|
+	| This file is for storing the credentials for third party services such
+	| as Stripe, Mailgun, Mandrill, and others. This file provides a sane
+	| default location for this type of information, allowing packages
+	| to have a conventional place to find your various credentials.
+	|
+	*/
+
+	'mailgun' => [
+		'domain' => 'sandbox722baa54b8614e1a9de2726232750e55.mailgun.org',
+		'secret' => 'ff05952822099a26c328e3d1472e59c6',
+	],
+
+	'mandrill' => [
+		'secret' => '',
+	],
+
+	'ses' => [
+		'key' => '',
+		'secret' => '',
+		'region' => 'us-east-1',
+	],
+
+	'stripe' => [
+		'model'  => 'App\User',
+		'secret' => '',
+	],
+
+];
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,269 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Anyplace -event for your days-</title>
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="{{asset('bower_components/bootstrap/dist/css/bootstrap.min.css')}}" type="text/css">
+
+    <!-- Custom Fonts -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="{{asset('bower_components/fontawesome/css/font-awesome.min.css')}}" type="text/css">
+
+    <!-- Plugin CSS -->
+    <link rel="stylesheet" href="{{asset('css/animate.min.css')}}" type="text/css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{asset('css/creative.css')}}" type="text/css">
+    
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body id="page-top">
+
+    <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top">AnyPlace</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a class="page-scroll" href="#about">About</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Services</a>
+                    </li>
+                    
+                    <li>
+                        <a class="page-scroll" href="#contact">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container-fluid -->
+    </nav>
+
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <br><br><br>
+                <h1 >AnyPlace</h1>
+                <hr class="line">
+                <h3>Cara baru update informasi Event & tempat seru disekitar Kamu </h3>
+                <a href="#about" class="page-scroll icon"><i class="fa fa-angle-double-down fa-5x icon"></i></a>
+            </div>
+        </div>
+    </header>
+
+    <section class="about bg-white" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading"><strong>AnyPlace</strong></h2>
+                    <hr class="light">
+                    <p class="text-black"> lorem ipsum blalalalalllalal</p>
+                </div>
+            </div> 
+
+            <div class="row line-icon">
+                <div class="col-md-1 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-camera-retro wow bounceIn text-black " data-wow-delay=".2s"></i>                       
+                    </div>
+                </div>
+                <div class=" col-md-1 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x  fa-film wow bounceIn text-black" data-wow-delay=".4s"></i>                        
+                    </div>
+                </div>
+                <div class=" col-md-1 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-gamepad wow bounceIn text-black" data-wow-delay=".5s"></i>                      
+                    </div>
+                </div>
+                <div class="col-md-1 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-heartbeat wow bounceIn text-black" data-wow-delay=".6s"></i>                        
+                    </div>
+                </div>
+                <div class="col-md-1 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x  fa-coffee wow bounceIn text-black " data-wow-delay=".8s"></i>                       
+                    </div>
+                </div>
+                <div class=" col-md-1 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-comments wow bounceIn text-black" data-wow-delay=".9s"></i>                        
+                    </div>
+                </div>
+                <div class=" col-md-1 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-users wow bounceIn text-black" data-wow-delay=".8s"></i>                      
+                    </div>
+                </div>                
+                <div class="col-md-1 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-smile-o wow bounceIn text-black " data-wow-delay=".6s"></i>                       
+                    </div>
+                </div>
+                <div class=" col-md-1 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-shopping-cart wow bounceIn text-black" data-wow-delay=".5s"></i>                        
+                    </div>
+                </div>
+                <div class=" col-md-1 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-cutlery wow bounceIn text-black" data-wow-delay=".4s"></i>                      
+                    </div>
+                </div>
+                <div class="col-md-1 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-music wow bounceIn text-black" data-wow-delay=".2s"></i>                        
+                    </div>
+                </div>
+
+            </div>          
+        </div>
+        
+    </section>
+
+   
+    <section id="services" class="bg-dark services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading"> Cek events seru di AnyPlace !!</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            
+            <div class="row">
+                <div class="col-lg-4 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-5x fa-map-marker wow bounceIn text-white " data-wow-delay=".2s"></i>
+                        <h3 class="text-green">Wisata Seru</h3>
+                        <p class="text-muted">Bertualang gak mesti ke luar negeri vro </p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-5x fa-shopping-cart wow bounceIn text-white " data-wow-delay=".2s"></i>
+                        <h3 class="text-green">Bazar & Diskon</h3>
+                        <p class="text-muted">Belanja Asik & gak perlu mahal</p>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-5x fa fa-cutlery wow bounceIn text-white" data-wow-delay=".2s"></i>
+                        &nbsp &nbsp 
+                        <i class="fa fa-5x fa fa-coffee wow bounceIn text-white" data-wow-delay=".2s"></i>
+                  
+                        <h3 class="text-green">Kuliner Hunter</h3>
+                        <p class="text-muted"> Kuliner yang Sehat,Enak, Murah & Asik </p>
+                    </div>                    
+                </div>
+               
+            </div>
+            <hr class="line">
+
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa fa-group wow bounceIn text-white " data-wow-delay=".2s"></i>
+                        <h3> Nongkrong Asik</h3>
+                        <p class="text-muted"></p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa fa-film wow bounceIn text-white" data-wow-delay=".2s"></i>
+                        <h3>Movies Update</h3>
+                        <p class="text-muted"></p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">                    
+                    <div class="service-box">
+                        <i class="fa fa-4x  fa-coffee wow bounceIn text-white  " data-wow-delay=".2s"></i>
+                        <h3 >Coffee Break</h3>
+                        <p class="text-muted"></p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-heart wow bounceIn text-red" data-wow-delay=".3s"></i>
+                        <h3>Romantis Area </h3>
+                        <p class="text-muted"></p>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+    </section>
+
+         
+<section id="contact" class="contact">
+        <div>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Info Lebih Lanjut Project <strong>Anyplace</strong></h2>
+                    <hr class="primary">
+                    <p>-----------------</p>
+                    <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".2s"></i>
+                   <hr class="primary">
+                    <form role="form" class="text-center" action="" method="POST">
+                      <div class="form-group">
+                        <div class="input-group">
+                          <input type="text" class="form-control" placeholder="Enter your email">
+                          <span class="input-group-btn">
+                            <a class="btn btn-success" type="submit">Go</a>
+                          </span>
+                        </div>
+                      </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+    
+    <!-- jQuery -->
+    <script src="{{asset('bower_components/jquery/dist/jquery.min.js')}}"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{{asset('bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+
+    <!-- Plugin JavaScript -->
+    <script src="{{asset('bower_components/jquery-easing-original/jquery.easing.min.js')}}"></script>
+    <script src="{{asset('bower_components/FitText.js/jquery.fittext.js')}}"></script>
+    <script src="{{asset('js/wow.min.js')}}"></script>
+
+    <!-- Custom Theme JavaScript -->
+    <script src="{{asset('js/creative.js')}}"></script>
+    
+</body>
+</html>
